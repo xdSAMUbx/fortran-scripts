ed4a4f861fd6ab449b14ebd0092ad5850ce29449
Samuel Calderon <samuelcalderon1202@hotmail.com>
2025-08-20 10:10:31 -0500

    pivoting

--- app/main.f90
+++ app/main.f90
@@ -2,11 +2,20 @@ program prueba
    use iso_fortran_env
    use mod_eye
    implicit none
-   integer, allocatable :: x(:,:)
-   integer :: n = 10
+   integer(int64) :: mat(3,3), vec(3), i,j
+   logical :: has_zero
+   mat = reshape([1,2,3,4,5,6,7,8,9],[3,3])
+   vec = 0
+   ! Recorrido por columnas
+   write(*,'(3(i0,1x))') mat
+   do concurrent (i = 1:3, j=1:2)
+      if (mat(i,j) == 0) then
+         vec = mat(:,j)
+         mat(:,j) = mat(:,j+1)
+         mat(:,j+1) = vec
+      end if
+   end do
 
-   x = eye(n)
-   write(*,'(10(i0, 1x))') x
-   deallocate(x)
-
+   write(*,'(3(i0,1x))') vec
+   write(*,'(3(i0,1x))') mat
 end program prueba
