8b06ca38546072415214e911ec454a6a326601c3
Samuel Calderon <samuelcalderon1202@hotmail.com>
2025-08-21 00:59:13 -0500

    final 1 points

--- app/main.f90
+++ app/main.f90
@@ -1,21 +1,10 @@
 program prueba
    use iso_fortran_env
-   use mod_eye
+   use mod_point
    implicit none
-
-   integer(int64) :: mat(3,3), vec(3), i,j
-   mat = reshape([1,2,3,4,5,6,7,8,9],[3,3])
-   vec = 0
-   ! Recorrido por columnas
-   write(*,'(3(i0,1x))') mat
-   do concurrent (i = 1:3, j=1:2)
-      if (mat(i,j) == 0) then
-         vec = mat(:,j)
-         mat(:,j) = mat(:,j+1)
-         mat(:,j+1) = vec
-      end if
-   end do
-
-   write(*,'(3(i0,1x))') vec
-   write(*,'(3(i0,1x))') mat
+   integer(int64) :: i,j
+   integer(int64) :: x(3)=[(i,i=1,3)], y(3)=[(j,j=1,3)]
+   type(iPoint2d),allocatable :: res(:)
+   res = point(x,y)
+   write(*,'(2(i0,1x))') res
 end program prueba
--- src/geoutils/point.f90
+++ src/geoutils/point.f90
@@ -2,138 +2,179 @@
 module mod_point
    use iso_fortran_env
    implicit none
+
+!------------------------------------------
+!------------- Definición de tipos --------
+!------------------------------------------
    type :: iPoint2d
-      integer(int64) :: x, y
+   integer(int64) :: x, y
    end type iPoint2d
 
    type, extends(iPoint2d) :: iPoint3d
-      integer(int64) :: z
+   integer(int64) :: z
    end type iPoint3d
 
    type, extends(iPoint3d) :: iPoint4d
-      integer(int64) :: t
+   integer(int64) :: t
    end type iPoint4d
 
    type :: rPoint2d
-      real(real128) :: x, y
+   real(real128) :: x, y
    end type rPoint2d
 
    type, extends(rPoint2d) :: rPoint3d
-      real(real128) :: z
+   real(real128) :: z
    end type rPoint3d
 
    type, extends(rPoint3d) :: rPoint4d
-      real(real128) :: t
+   real(real128) :: t
    end type rPoint4d
+
+
+!------------------------------------------
+!------------- Interfaz pública -----------
+!------------------------------------------
+   interface point
+      module procedure iPoint2ds
+      module procedure iPoint2darr
+      module procedure iPoint3ds
+      module procedure iPoint3darr
+      module procedure iPoint4ds
+      module procedure iPoint4darr
+      module procedure rPoint2ds
+      module procedure rPoint2darr
+      module procedure rPoint3ds
+      module procedure rPoint3darr
+      module procedure rPoint4ds
+      module procedure rPoint4darr
+   end interface point
 
    contains
 
+!------------------------------------------
+!------------- Constructores --------------
+!------------------------------------------
+! integer part
    pure function iPoint2ds(x, y) result(res)
-      implicit none
+   implicit none
+   integer(int64), intent(in) :: x, y
+   type(iPoint2d) :: res
 
-      integer(int64), intent(in) :: x, y
-      type(iPoint2d) :: res
-
-      res%x = x
-      res%y = y
+   res = iPoint2d(x, y)
    end function iPoint2ds
 
    pure function iPoint2darr(x, y) result(res)
-      implicit none
+   implicit none
+   integer(int64), intent(in) :: x(:), y(:)
+   type(iPoint2d), allocatable :: res(:)
+   integer :: i
 
-      integer(int64), intent(in) :: x(:), y(:)
-      integer(int64), allocatable :: res
+   allocate(res(size(x)))
+   do concurrent (i=1:size(x))
+      res(i) = iPoint2d(x(i), y(i))
+   end do
    end function iPoint2darr
 
    pure function iPoint3ds(x, y, z) result(res)
-      implicit none
+   implicit none
+   integer(int64), intent(in) :: x, y, z
+   type(iPoint3d) :: res
 
-      integer(int64), intent(in) :: x, y, z
-      type(iPoint3d)  :: res
-
-      res%x = x
-      res%y = y
-      res%z = z
+   res = iPoint3d(x, y, z)
    end function iPoint3ds
 
    pure function iPoint3darr(x, y, z) result(res)
-      implicit none
+   implicit none
+   integer(int64), intent(in) :: x(:), y(:), z(:)
+   type(iPoint3d), allocatable :: res(:)
+   integer :: i
 
-      integer(int64), intent(in) :: x(:), y(:), z(:)
-      integer(int64), allocatable :: res
+   allocate(res(size(x)))
+   do concurrent (i=1:size(x))
+      res(i) = iPoint3d(x(i), y(i), z(i))
+   end do
    end function iPoint3darr
 
    pure function iPoint4ds(x, y, z, t) result(res)
-      implicit none
+   implicit none
+   integer(int64), intent(in) :: x, y, z, t
+   type(iPoint4d) :: res
 
-      integer(int64), intent(in) :: x, y, z, t
-      type(iPoint4d)  :: res
-
-      res%x = x
-      res%y = y
-      res%z = z
-      res%t = t
+   res = iPoint4d(x, y, z, t)
    end function iPoint4ds
 
    pure function iPoint4darr(x, y, z, t) result(res)
-      implicit none
+   implicit none
+   integer(int64), intent(in) :: x(:), y(:), z(:), t(:)
+   type(iPoint4d), allocatable :: res(:)
+   integer :: i
 
-      integer(int64), intent(in) :: x(:), y(:), z(:), t(:)
-      integer(int64), allocatable :: res
+   allocate(res(size(x)))
+   do concurrent (i=1:size(x))
+      res(i) = iPoint4d(x(i), y(i), z(i), t(i))
+   end do
    end function iPoint4darr
 
+! real part
    pure function rPoint2ds(x, y) result(res)
-      implicit none
+   implicit none
+   real(real128), intent(in) :: x, y
+   type(rPoint2d) :: res
 
-      real(real128), intent(in) :: x, y
-      type(rPoint2d) :: res
-
-      res%x = x
-      res%y = y
+   res = rPoint2d(x, y)
    end function rPoint2ds
 
    pure function rPoint2darr(x, y) result(res)
-      implicit none
+   implicit none
+   real(real128), intent(in) :: x(:), y(:)
+   type(rPoint2d), allocatable :: res(:)
+   integer :: i
 
-      real(real128), intent(in) :: x(:), y(:)
-      real(real128), allocatable :: res
+   allocate(res(size(x)))
+   do concurrent (i=1:size(x))
+      res(i) = rPoint2d(x(i), y(i))
+   end do
    end function rPoint2darr
 
    pure function rPoint3ds(x, y, z) result(res)
-      implicit none
+   implicit none
+   real(real128), intent(in) :: x, y, z
+   type(rPoint3d) :: res
 
-      real(real128), intent(in) :: x, y, z
-      type(rPoint3d)  :: res
-
-      res%x = x
-      res%y = y
-      res%z = z
+   res = rPoint3d(x, y, z)
    end function rPoint3ds
 
    pure function rPoint3darr(x, y, z) result(res)
-      implicit none
+   implicit none
+   real(real128), intent(in) :: x(:), y(:), z(:)
+   type(rPoint3d), allocatable :: res(:)
+   integer :: i
 
-      real(real128), intent(in) :: x(:), y(:), z(:)
-      real(real128), allocatable :: res
+   allocate(res(size(x)))
+   do concurrent (i=1:size(x))
+      res(i) = rPoint3d(x(i), y(i), z(i))
+   end do
    end function rPoint3darr
 
    pure function rPoint4ds(x, y, z, t) result(res)
-      implicit none
+   implicit none
+   real(real128), intent(in) :: x, y, z, t
+   type(rPoint4d) :: res
 
-      real(real128), intent(in) :: x, y, z, t
-      type(rPoint4d)  :: res
-
-      res%x = x
-      res%y = y
-      res%z = z
-      res%t = t
+   res = rPoint4d(x, y, z, t)
    end function rPoint4ds
 
    pure function rPoint4darr(x, y, z, t) result(res)
-      implicit none
+   implicit none
+   real(real128), intent(in) :: x(:), y(:), z(:), t(:)
+   type(rPoint4d), allocatable :: res(:)
+   integer :: i
 
-      real(real128), intent(in) :: x(:), y(:), z(:), t(:)
-      real(real128), allocatable :: res
+   allocate(res(size(x)))
+   do concurrent (i=1:size(x))
+      res(i) = rPoint4d(x(i), y(i), z(i), t(i))
+   end do
    end function rPoint4darr
+
 
 end module mod_point
