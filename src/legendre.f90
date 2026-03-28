0033218d823e7ef76baf44ac9fe231a38447d996
Samuel Calderon <samuelcalderon1202@hotmail.com>
2026-03-27 20:57:26 -0500

    legendre normalizado

--- src/legendre.f90
+++ src/legendre.f90
@@ -9,33 +9,48 @@ contains
         val1 = val1 * (pi / 180.0)
     end subroutine to_radians    
 
-    pure subroutine leg_func(n, theta, p_n)
+    subroutine legendre_function(n, theta, p_n)
         implicit none
+        integer(int32), intent(in) :: n
         real(real64), intent(in) :: theta
-        integer(int16), intent(in) :: n
         real(real64), intent(out) :: p_n
-        integer(int16) :: i
-        real(real64) :: p_n0, p_n1
+
+        integer(int32) :: i
+        real(real64) :: p_n0, p_n1, ct, ri
         
-        if ( n == 0 ) then
+        ct = theta
+        call to_radians(ct)
+        ct = cos(ct)
+
+        select case(n)
+        case (0)
             p_n = 1.0_real64
-            return
-        else if ( n == 1 ) then
-            p_n = cos(theta)
-            return
-        end if
-
-        p_n0 = 1.0_real64
-        p_n1 = cos(theta)
-
-        do i = 2, n
-            p_n = - ((i - 1.0_real64) / i) * p_n0 &
-                + ((2 * i - 1.0_real64) / i) * cos(theta) * p_n1
-            p_n0 = p_n1
-            p_n1 = p_n
-        end do
+        case (1)
+            p_n = ct
+        case default
+            p_n0 = 1.0_real64
+            p_n1 = ct
+            do i = 2, n
+                ri = real(i, real64)
+                p_n = ((2.0_real64*ri - 1.0_real64) * ct * p_n1 &
+                    - (ri - 1.0_real64) * p_n0) / ri
+                p_n0 = p_n1
+                p_n1 = p_n 
+            end do
+        end select
         
-    end subroutine leg_func
+    end subroutine legendre_function
+
+    subroutine normalized_legendre_function(n, theta, p_n)
+        implicit none
+        integer(int32), intent(in) :: n
+        real(real64), intent(in) :: theta
+        real(real64), intent(out) :: p_n
+
+        call legendre_function(n, theta, p_n)
+        p_n = sqrt(2.0_real64 * real(n, real64) + 1.0_real64) * p_n
+
+    end subroutine normalized_legendre_function
 end module prueba_legendre
 
 program physical_geodesy
@@ -45,8 +60,7 @@ program physical_geodesy
     real(real64) :: prueba, ang
 
     ang = 10.0_real64
-    call to_radians(ang)
-    call leg_func(10000_int16, ang, prueba)
+    call normalized_legendre_function(100000_int32, ang, prueba)
 
     write(*,*) "La respuesta es: ", prueba
     
